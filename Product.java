6df0eed02df1215d0be9b3801ae58eca79a52361
HasnaFasinVk <151716540+HasnaFasinVk@users.noreply.github.com>
2024-04-01 14:51:25 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,66 +1,41 @@
-
-
-
 import java.util.Scanner;
-
 class Product {
+int pcode;
+String pname;
+int price;
+Product(int code, String name, int cost) {
+pcode = code;
+pname = name;
+price = cost;
+}
+}
+class Mainproduct{
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.println("Enter the total number of products:");
+int n = input.nextInt();
+Product[] products = new Product[n];
+for(int i=0; i<n; i++) {
+System.out.println("Enter product code:");
+int code = input.nextInt();
+input.nextLine(); // consume newline left-over
+System.out.println("Enter product name:");
+String name = input.nextLine();
+System.out.println("Enter product price:");
+int cost = input.nextInt();
+products[i] = new Product(code, name, cost);
+}
+int minPrice = products[0].price;
+Product minProduct = products[0];
+for(int i=1; i<n; i++) {
+if(products[i].price < minPrice) {
+minPrice = products[i].price;
+minProduct = products[i];
+}
+}
+System.out.println("The product with the lowest price is: " + minProduct.pname + " with a price of: " + minProduct.price);
+}
+}
 
-void product (int pcode, String pname, int price) (
 
-System.out.println("pcode:"+pcode);
 
-System.out.println("pname:"+pname);
-
-System.out.println("price:"+price);
-
-int products (int price) (
-
-return price;
-
-}}
-
-class prodmain(
-
-public static void main(String args[]) {
-
-Scanner input-new Scanner(System.in);
-
-System.out.println("enter pcode, pname,price");
-
-Product pl-new Product();
-
-int cl-input.nextInt();
-
-String nl-input.nextLine();
-
-int rl-input.nextInt();
-
-pl.product (cl,nl,rl);
-
-System.out.println("enter pcode,pname,price");
-
-Product o * 2 = neW Product();
-
-int c2-input.nextInt();
-
-String n2-input.nextLine();
-
-int r2-input.nextInt();
-
-p2.product ( c 2,n2) r2);
-
-System.out.println("enter pcode, pname,price");
-
-Product p3-new Product();
-
-int c3-input.nextInt();
-
-String n3-input.nextLine();
-
-int r3-input.nextInt();
-
-p3.product ( c 3,n3,x3) ;
-
-al-pl.products (
-
-}}
