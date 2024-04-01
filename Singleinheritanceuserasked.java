8e43343eb2191fc0d2c85a42e16d59a31b9ca808
HasnaFasinVk <151716540+HasnaFasinVk@users.noreply.github.com>
2024-04-01 13:58:59 +0530

    Update and rename Single.java to Singleinheritanceuserasked.java

--- Singleinheritanceuserasked.java
+++ Singleinheritanceuserasked.java
@@ -1,51 +1,37 @@
 import  java.util.Scanner;
-
 class Shape {
-
 int width;
-
 int length;
-
 Shape (int w,int h) {
-
-width-w;
-
+width=w;
 length=h;
 
 }}
 
 class Rectangle extends Shape
-
 {
-
 Rectangle(int w,int h) {
-
 super(w,h);}
-
 void display(){
 
-2 System.out.println("width:"+width+"length:"+length);
-
+System.out.println("width:"+width+"length:"+length);
+}
 void area ()
-
+{
 int area=width*length;
-
-2 System.out.println("area"+area);
-
-(}}
+System.out.println("area"+area);
+}}
 
 class area{
-
 public static void main(String[] args){
-
-Scanner input-new Scanner(System.in);
-
+Scanner input=new Scanner(System.in);
 System.out.println("enter length and breadth");
-
-int 1-input.nextInt(); int b-input.nextInt();
-
-Rectangle obj-new Rectangle(1,b);
-
+int l=input.nextInt();
+int b=input.nextInt();
+Rectangle obj=new Rectangle(l,b);
 obj.display();
-
 obj.area();
+}
+}
+
+
